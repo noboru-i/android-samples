5135994f7b44f6aeba7bbd6c4c5649183fe7c3be
noboru-i <ishikura.noboru@gmail.com>
2016-05-06 22:15:24 +0900

    create main list

--- app/src/main/java/hm/orz/chaos114/android/androidsamples/MainActivity.java
+++ app/src/main/java/hm/orz/chaos114/android/androidsamples/MainActivity.java
@@ -1,13 +1,33 @@
 package hm.orz.chaos114.android.androidsamples;
 
-import android.support.v7.app.AppCompatActivity;
+import android.databinding.DataBindingUtil;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.Toast;
+
+import hm.orz.chaos114.android.androidsamples.databinding.ActivityMainBinding;
 
 public class MainActivity extends AppCompatActivity {
+    private static final String[] screens = {
+            "NestedScrollView"
+    };
+
+    ActivityMainBinding binding;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+
+        binding = DataBindingUtil.setContentView(this, R.layout.activity_main);
+        binding.list.setAdapter(new ArrayAdapter<>(this, R.layout.view_main_row, screens));
+        binding.list.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                Toast.makeText(MainActivity.this, screens[position], Toast.LENGTH_SHORT).show();
+            }
+        });
     }
 }
